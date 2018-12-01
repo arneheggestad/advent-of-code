4ca204cd92b572ebf91b4b46a58d0bddf2efa9c7
Arne Heggestad <arne.heggestad@gmail.com>
2018-12-01 10:47:41 -0500

    Logic seems okay for the second part

--- 2018/day01.py
+++ 2018/day01.py
@@ -1,12 +1,29 @@
 inputFile = open('./data/input01.txt')
 
-freq = 0
+startingFreq = 0
+startingFreqs = []
 
-print('starting frequency: ', freq)
+print('starting frequency: ', startingFreq)
 
-changes = inputFile.read()
-changes = changes.splitlines()
-for c in changes:
-  freq = freq + int(c)
+# changes = inputFile.read()
+# changes = changes.splitlines()
+# changes = [3, 3, 4, -2, -4]
+changes = [7, 7, -2, -7, -4]
 
-print('ending frequency: ', freq)
+def doChanges(freq, freqs, dupe):
+  for c in changes:
+    if freq in freqs:
+      print('duplicate frequency: ', freq)
+      freqs.append(freq)
+      dupe = True
+    else:
+      freqs.append(freq)
+    freq = freq + int(c)
+
+  return(freq, freqs, dupe)
+
+(endingFreq, existingFreqs, duplicateFound) = doChanges(startingFreq, startingFreqs, False)
+print('ending frequency from fx: ', endingFreq)
+while duplicateFound is False:
+  (endingFreq, existingFreqs, duplicateFound) = doChanges(endingFreq, existingFreqs, duplicateFound)
+  print('ending frequency from fx: ', endingFreq)
