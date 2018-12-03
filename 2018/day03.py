e7fa4369225313a478c3dbc23462004c4e7da348
Arne Heggestad <arne.heggestad@gmail.com>
2018-12-03 14:52:44 -0500

    added test claims

---
+++ 2018/day03.py
@@ -0,0 +1,8 @@
+inputFile = open('./data/input02.txt')
+
+testClaims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
+claims = testClaims
+
+# claims = inputFile.read()
+# claims = claims.splitlines()
+
