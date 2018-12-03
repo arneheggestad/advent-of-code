e9ac24340708bd9e62d1e152b7d56342526d8d8b
Arne Heggestad <arne.heggestad@gmail.com>
2018-12-03 15:41:11 -0500

    claiming area with test claims

--- 2018/day03.py
+++ 2018/day03.py
@@ -3,10 +3,10 @@ import re
 
 testClaims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
 
-# claims = testClaims
+claims = testClaims
 
-claims = inputFile.read()
-claims = claims.splitlines()
+# claims = inputFile.read()
+# claims = claims.splitlines()
 
 def parseClaim(claim):
   regex = r"#(\d*) @ (\d*),(\d*): (\d*)x(\d*)"
@@ -19,6 +19,22 @@ def parseClaim(claim):
   height = int(height)
   return (claimNumber, xStart, yStart, width, height)
 
+claimedArea = []
+conflicts = []
+
 for claim in claims:
   parsedClaim = (parseClaim(claim))
-  print(parsedClaim)
+  # print(parsedClaim)
+  (claimNumber, xStart, yStart, width, height) = parsedClaim
+
+  for i in range(0, width):
+    for j in range(0, height):
+      newClaim = (xStart + i, yStart + j)
+      if newClaim in claimedArea:
+        conflicts.append({'claim': claimNumber, 'coords': newClaim})
+      else:
+        claimedArea.append(newClaim)
+
+print(claimedArea)
+print(conflicts)
+print('duplicate area: ', len(conflicts))
